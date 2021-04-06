84db8e14ba7723bbc76cb6d4e3720e7d49ba490b
Ritwikrajmishra <76164118+Ritwikrajmishra@users.noreply.github.com>
2021-04-06 15:08:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function getpara1()
+{
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++)
+    {
+inputs.push(document.getElementById("para1_input_box_" + i).value);
+inputs.join(". ");
+    }
+    document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
+}
+function getpara2()
+{
+    var inputs = [];
+    for(var i = 1 ; i <=6 ; i++)
+    {
+inputs.push(document.getElementById("para2_input_box_" + i).value);
+inputs.join(". ");
+    }
+    document.getElementById("showParagraph2").innerHTML = inputs.join(". ");
+}
